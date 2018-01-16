5cdeceba980dea8a62bb0d84d1c6cf2fb3c4c5a1
nb6u7 <b6u728@gmail.com>
2018-01-17 02:07:40 +0900

    Implement Insertion Sort

---
+++ Sort/insertion_sort.rb
@@ -0,0 +1,21 @@
+# Input format is as follows:
+# 6
+# 5 2 4 6 1 3
+
+n = gets.chomp.to_i
+arr = gets.chomp.split(' ').map(&:to_i)
+
+def insertion_sort(arr, n)
+  (1..n-1).each do |i|
+    v = arr[i]
+    j = i - 1
+    while j >= 0 && arr[j] > v
+      arr[j + 1] = arr[j]
+      j -= 1
+    end
+    arr[j + 1] = v
+    print "#{arr.join(' ')}\n"
+  end
+end
+
+insertion_sort(arr, n)
