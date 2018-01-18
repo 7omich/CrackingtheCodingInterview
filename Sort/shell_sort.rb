4a67d9f722cc447558402190ffda63e953ddab09
nb6u7 <b6u728@gmail.com>
2018-01-18 21:30:18 +0900

    Implement Shell Sort

---
+++ Sort/shell_sort.rb
@@ -0,0 +1,38 @@
+# Time complexity: O(n^1.25)
+# Input format is as follows:
+# 5
+# 5 1 4 3 2
+
+n = gets.chomp.to_i
+arr = gets.chomp.split(' ').map(&:to_i)
+
+@count = 0
+@g_arr = []
+
+def insertion_sort(arr, n, g)
+  (g..n-1).each do |i|
+    v = arr[i]
+    j = i - g
+    while j >= 0 && arr[j] > v
+      arr[j + g] = arr[j]
+      j -= g
+      @count += 1
+    end
+    arr[j + g] = v
+  end
+end
+
+def shell_sort(arr, n)
+  h = 1
+  while h <= n
+    @g_arr.unshift(h)
+    h = h * 3 + 1
+  end
+
+  @g_arr.each do |g|
+    insertion_sort(arr, n, g)
+  end
+end
+
+shell_sort(arr, n)
+print "#{@g_arr.join(' ')}\n#{@count}\n#{arr.join(' ')}\n"
