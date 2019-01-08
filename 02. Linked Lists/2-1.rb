5780676d09808b43a7d7ed910f2043abe18451db
7omich <b6u728@gmail.com>
2019-01-09 03:01:37 +0900

    Previous works (My heart broken)

---
+++ 02. Linked Lists/2-1.rb
@@ -0,0 +1,12 @@
+class Node
+	attr_accessor :value, :next_node
+	def initialize(value, next_node)
+		@value = value
+		@next_node = next_node
+	end
+end
+
+class LinkedList
+	def remove_dups(self)
+	end
+end
